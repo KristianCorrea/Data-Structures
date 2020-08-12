61b62af1bdbd7e3d33a53e2f18d85cf9d95977d9
Kristian Correa <kristiangcorrea@live.com>
2020-08-12 16:29:37 -0400

    Doubly lInked list implemented

--- stack/stack.py
+++ stack/stack.py
@@ -10,39 +10,40 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
-class Stack:
-    def __init__(self):
-        self.size = 0
-        self.storage = []
-
-    def __len__(self):
-        return self.size
-
-    def push(self, value):
-        self.storage.append(value)
-        self.size += 1
-
-    def pop(self):
-        if self.size == 0:
-            return None
-        self.size -= 1
-        return self.storage.pop(len(self.storage)-1)
-        
-
 # class Stack:
 #     def __init__(self):
 #         self.size = 0
-#         self.storage = LinkedList()
+#         self.storage = []
 
 #     def __len__(self):
 #         return self.size
 
 #     def push(self, value):
-#         self.storage.add_to_tail(value)
+#         self.storage.append(value)
 #         self.size += 1
 
 #     def pop(self):
-#         if self.size < 1:
+#         if self.size == 0:
 #             return None
 #         self.size -= 1
-#         return self.storage.remove_tail()
+#         return self.storage.pop(len(self.storage)-1)
+        
+from singly_linked_list import LinkedList
+
+class Stack:
+    def __init__(self):
+        self.size = 0
+        self.storage = LinkedList()
+
+    def __len__(self):
+        return self.size
+
+    def push(self, value):
+        self.storage.add_to_tail(value)
+        self.size += 1
+
+    def pop(self):
+        if self.size < 1:
+            return None
+        self.size -= 1
+        return self.storage.remove_tail()
